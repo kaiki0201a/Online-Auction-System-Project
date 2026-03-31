13d60ca5b93b40896e0da1989df1df434f529351
hoangtl2 <hoangviethoang317@gmail.com>
2026-03-31 21:41:23 +0700

    31-03-2026 Fix Bug Bidder

--- src/main/java/com/auction/model/Bidder.java
+++ src/main/java/com/auction/model/Bidder.java
@@ -1,13 +1,13 @@
 package com.auction.model;
 import java.util.ArrayList;
 import java.util.List;
-public class Bidder {
+public class Bidder extends User{
     private double balance; 
     private List<String> jAuction; // Danh sách id các phiên đấu giá đá tham gia
     public Bidder(String userName, String passWord, String email,double balance){
         super(userName, passWord, email);
         this.balance = balance;
-        this.jAuction = new Arraylist<>();
+        this.jAuction = new ArrayList<>();
     }
     public double getbalance(){
         return this.balance;
@@ -25,17 +25,17 @@ public class Bidder {
         System.out.println("========== BIDDER DASHBOARD ==========");
         System.out.println("Xin chào, " + this.getUserName());
         System.out.println("Email: " + this.getEmail());
-        System.out.println("Số dư hiện tại: $" + this.accountBalance);
+        System.out.println("Số dư hiện tại: $" + this.balance);
         System.out.println("======================================");
     }
     public boolean placeBid(Auction auction, double amount)
     {
         if(amount > this.getbalance()){
-            System.out.println("Số dư không đủ! Bạn còn : " + this.getbalance())
+            System.out.println("Số dư không đủ! Bạn còn : " + this.getbalance());
             return false;
         }
         boolean isSuccess = auction.placeBid(this,amount);
-        if(isSucces){
+        if(isSuccess){
             System.out.println("Bidder "+this.getUserName()+" đã đấu giá thành công " + amount + "vào phiên " + auction.getAuctionId());
         }
         return isSuccess;
@@ -45,7 +45,7 @@ public class Bidder {
             System.out.println("Lỗi : Với số dư của bạn chỉ có thể cài Auto Bid với mức tối đa là :" +this.balance);
             return;
         }
-        System.out.println("Thành công: " this.getUserName() + " đã cài đặt Auto Bid thành công cho phiên giao dịch: " + auction.getAuctionId());
+        System.out.println("Thành công: " + this.getUserName() + " đã cài đặt Auto Bid thành công cho phiên giao dịch: " + auction.getAuctionId());
         System.out.println("Trả giá tự động lên tối đa: "+ maxBid +" với bước nhảy: "+ increment);
     }
     
