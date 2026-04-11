26d000e38596462c2c7067360a3eb6edf80c87bc
trandiep2910 <25020123@vnu.edu.vn>
2026-04-11 21:07:06 +0700

    interfece-dao

--- src/main/java/com/auction/dao/IGenericDAO.java
+++ src/main/java/com/auction/dao/IGenericDAO.java
@@ -3,9 +3,9 @@ package com.auction.dao;
 import java.util.List;
 
 public interface IGenericDAO<T> {
-    void save(T obj);
-    void update(T obj);
-    void delete(String id);
+    boolean save(T obj);
+    boolean update(T obj);
+    boolean delete(String id);
     T findById(String id);
     List<T> findAll();
 }
