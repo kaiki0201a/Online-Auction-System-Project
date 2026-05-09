9836d38b8ee1c9b1ec4648788338c08e5945e348
fushi <kaiki0201a@gmail.com>
2026-05-10 00:32:44 +0700

    Fix AppTest - xoá test rác

--- src/test/java/com/auction/model/AppTest.java
+++ src/test/java/com/auction/model/AppTest.java
@@ -5,8 +5,4 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-    @Test
-    public void testAdd() {
-        assertEquals(15, App.add(5, 10));
-    }
 }
