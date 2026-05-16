39f23509bc0d8d13811bef60b2f1fe056fb0c6f9
hoangtl2 <hoangviethoang317@gmail.com>
2026-05-16 17:25:08 +0700

    PriorityQueue update, Autobidrule fix for Priority + Fix time Anti-sniping

--- src/main/java/com/auction/utils/AuctionObserver.java
+++ src/main/java/com/auction/utils/AuctionObserver.java
@@ -1,7 +1,9 @@
 package com.auction.utils;
 
+import com.auction.model.BidTransaction;
 // Hợp đồng "Lắng nghe cập nhật"
 public interface AuctionObserver {
     // Hàm này sẽ bị ép phải chạy mỗi khi phiên đấu giá có biến động
     void update(String message);
+    void onNewBidPlaced(BidTransaction transaction);
 }
